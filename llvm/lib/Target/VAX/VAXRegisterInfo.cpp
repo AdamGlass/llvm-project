798d7597061422f59eb7906874e6d45d8f875e81
Adam Glass <adamglass@spottedfoobar.com>
2024-11-03 12:10:37 -0800

    connect enough to invoke tablegen

---
+++ llvm/lib/Target/VAX/VAXRegisterInfo.cpp
@@ -0,0 +1,44 @@
+//===-- VAXRegisterInfo.cpp - VAX Register Information ----------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the VAX implementation of the MRegisterInfo class.
+//
+//===----------------------------------------------------------------------===//
+
+#include "VAXRegisterInfo.h"
+#include "VAX.h"
+#if 0
+#include "VAXInstrInfo.h"
+#include "VAXMachineFunctionInfo.h"
+#include "VAXSubtarget.h"
+#endif
+#include "llvm/ADT/BitVector.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/CodeGen/MachineFrameInfo.h"
+#include "llvm/CodeGen/MachineFunction.h"
+#include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/MachineModuleInfo.h"
+#include "llvm/CodeGen/MachineRegisterInfo.h"
+#include "llvm/CodeGen/RegisterScavenging.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Type.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/MathExtras.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/CodeGen/TargetFrameLowering.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Target/TargetOptions.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "vax-reg-info"
+
+#define GET_REGINFO_TARGET_DESC
+#include "VAXGenRegisterInfo.inc"
+
