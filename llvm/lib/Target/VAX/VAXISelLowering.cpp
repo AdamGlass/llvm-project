66495052b13d80084575b722fdc178f594ff4705
Adam Glass <adamglass@spottedfoobar.com>
2024-11-09 20:40:31 -0800

    more skeleton

---
+++ llvm/lib/Target/VAX/VAXISelLowering.cpp
@@ -0,0 +1,84 @@
+//===-- VAXISelLowering.cpp - VAX DAG Lowering Implementation ---------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the VAXTargetLowering class.
+//
+//===----------------------------------------------------------------------===//
+
+#include "VAXISelLowering.h"
+#include "VAX.h"
+#include "VAXMachineFunctionInfo.h"
+#include "VAXSubtarget.h"
+#include "VAXTargetMachine.h"
+#include "VAXTargetObjectFile.h"
+#include "llvm/CodeGen/CallingConvLower.h"
+#include "llvm/CodeGen/MachineFrameInfo.h"
+#include "llvm/CodeGen/MachineFunction.h"
+#include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/MachineJumpTableInfo.h"
+#include "llvm/CodeGen/MachineRegisterInfo.h"
+#include "llvm/CodeGen/ValueTypes.h"
+#include "llvm/IR/CallingConv.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/GlobalAlias.h"
+#include "llvm/IR/GlobalVariable.h"
+#include "llvm/IR/Intrinsics.h"
+//#include "llvm/IR/IntrinsicsVAX.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/KnownBits.h"
+#include "llvm/Support/raw_ostream.h"
+#include <algorithm>
+
+using namespace llvm;
+
+#define DEBUG_TYPE "vax-lower"
+
+const char *VAXTargetLowering::
+getTargetNodeName(unsigned Opcode) const
+{
+  switch ((VAXISD::NodeType)Opcode)
+  {
+    case VAXISD::FIRST_NUMBER      : break;
+  }
+  return nullptr;
+}
+
+VAXTargetLowering::VAXTargetLowering(const TargetMachine &TM,
+                                         const VAXSubtarget &Subtarget)
+    : TargetLowering(TM), TM(TM), Subtarget(Subtarget) {
+
+  // Set up the register classes.
+  addRegisterClass(MVT::i32, &VAX::VRegsRegClass);
+
+  // Compute derived properties from the register classes
+  computeRegisterProperties(Subtarget.getRegisterInfo());
+
+  setStackPointerRegisterToSaveRestore(VAX::SP);
+
+  // Use i32 for setcc operations results
+  setBooleanContents(ZeroOrOneBooleanContent);
+
+  MaxStoresPerMemset = MaxStoresPerMemsetOptSize = 4;
+  MaxStoresPerMemmove = MaxStoresPerMemmoveOptSize
+    = MaxStoresPerMemcpy = MaxStoresPerMemcpyOptSize = 2;
+
+  setMinFunctionAlignment(Align(4));
+  setPrefFunctionAlignment(Align(4));
+
+  // This target doesn't implement native atomics.
+  setMaxAtomicSizeInBitsSupported(0);
+}
+
+
+//===----------------------------------------------------------------------===//
+//                           VAX Inline Assembly Support
+//===----------------------------------------------------------------------===//
+
