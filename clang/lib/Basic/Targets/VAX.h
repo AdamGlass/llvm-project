bbb5cbb006f263b719d81b14676cb0a1cf5910c1
Adam Glass <adamglass@spottedfoobar.com>
2024-11-11 23:47:15 -0800

    specify datalayout

--- clang/lib/Basic/Targets/VAX.h
+++ clang/lib/Basic/Targets/VAX.h
@@ -36,6 +36,7 @@ public:
     IntPtrType = SignedInt;
     WCharType = SignedInt;
     WIntType = SignedInt;
+    resetDataLayout("e-m:e-p:32:32-i1:8:32-i8:8:32-i16:16:32-i64:32-f64:32-a:0:32-n32");
   }
 
   void getTargetDefines(const LangOptions &Opts,
